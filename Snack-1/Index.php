da01114dcbad93625def9794055ac1e1ecac407b
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-22 16:47:35 +0200

    conclusione snack-1

--- Snack-1/Index.php
+++ Snack-1/Index.php
@@ -36,11 +36,26 @@ Olimpia Milano - Cantù | 55-60 -->
         ],
     ];
 
-    for($i = 0; $i < count($partite); $i++) {
-        echo $partite[$i]['SquadraDiCasa'] . " - ". $partite[$i]['SquadraOspite']. " | ". $partite[$i]['puntiCasa']. "-". $partite[$i]['puntiOspite'] ;
-    }
-
-
-
+    // for($i = 0; $i < count($partite); $i++) {
+    //     echo $partite[$i]['SquadraDiCasa'] . " - ". $partite[$i]['SquadraOspite']. " | ". $partite[$i]['puntiCasa']. "-". $partite[$i]['puntiOspite'] ;
+    // }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+<body>
+    <h1>Risultati Delle ultime partite:</h1>
+    <ul>
+        <?php for($i = 0; $i < count($partite); $i++) { ?>
+            <li><?php echo $partite[$i]['SquadraDiCasa'] . " - ". $partite[$i]['SquadraOspite']. " | ". $partite[$i]['puntiCasa']. "-". $partite[$i]['puntiOspite'] ; ?></li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
